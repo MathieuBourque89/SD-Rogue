506d9862d5135dddab564cc735a9203f2ad6ca7b
MathieuBourque89 <bourque.mathieu819@gmail.com>
2026-04-08 15:14:00 -0300

    Added item and gold class. Fixed arrow functionality. Gold is now displayed on screen

---
+++ RogueLib/Dungeon/Item.cs
@@ -0,0 +1,26 @@
+﻿using RogueLib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLib.Dungeon;
+
+public abstract class Item : IDrawable
+{
+    public Vector2 Pos { get; set; }
+    public char Glyph => _glyph;
+    protected char _glyph;
+    protected ConsoleColor _color;
+
+    public Item(char glyph, Vector2 pos, ConsoleColor color = ScreenBuff._notAColor)
+    {
+        _color = color;
+        _glyph = glyph;
+        Pos = pos;
+    }
+
+    public void Draw(IRenderWindow disp)
+    {
+        disp.Draw(_glyph, Pos, _color);
+    }
+}
---
+++ RougeLikeGame/Levels/Gold.cs
@@ -0,0 +1,16 @@
+﻿using RogueLib.Dungeon;
+using RogueLib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SandBox01.Levels;
+
+internal class Gold : Item
+{
+    public int amount { get; init; }
+    public Gold(Vector2 pos, int amt) : base('*', pos, ConsoleColor.Yellow)
+    {
+        amount = amt;
+    }
+}
