5071fd68b2a260256bdf61d8cf8345119c35dc33
MathieuBourque89 <bourque.mathieu819@gmail.com>
2026-04-16 23:58:58 -0300

    trap deal damage and clear, players get an RIP screen if they die

--- RougeLikeGame/Levels/Level.cs
+++ RougeLikeGame/Levels/Level.cs
@@ -1,3 +1,4 @@
+using RogueLib;
 using RogueLib.Dungeon;
 using RogueLib.Engine;
 using RogueLib.Utilities;
@@ -37,6 +38,7 @@ public class Level : Scene
 
     protected TileSet _discovered; // tiles the player has seen
     protected TileSet _inFov;      // current fov of player
+    protected TileSet _onPlayer;
     List<Enemy> _enemies;
 
    protected List<Item> _items;
@@ -100,6 +102,7 @@ public class Level : Scene
     protected void updateDiscovered()
     {
         _inFov = fovCalc(_player!.Pos, _senseRadius);
+        _onPlayer = fovCalc(_player!.Pos, _senseRadius - 3);
 
       if (_discovered is null)
          _discovered = new TileSet();
@@ -143,73 +146,88 @@ public class Level : Scene
       drawEnemies(disp);
       drawSpecialTiles(disp);
       disp.Draw(_player.HUD, new Vector2(0, 24), ConsoleColor.Green);
-   }
+      
+      if (_player._hp <= 0)
+         disp.Draw(DungeonConfig.RIP, ConsoleColor.Red);
+    }
 
-   public override void DoCommand(Command command) {
-      // player ctl  
-      if (command.Name == "up") {
-         MovePlayer(Vector2.N);
-      } else if (command.Name == "down") {
-         MovePlayer(Vector2.S);
-      } else if (command.Name == "left") {
-         MovePlayer(Vector2.W);
-      } else if (command.Name == "right") {
-         MovePlayer(Vector2.E);
-      } // game ctl      
-      else if (command.Name == "quit") {
-         _levelActive = false;
-      }
-   }
+    public override void DoCommand(Command command) {
+       // player ctl  
+       if (command.Name == "up") {
+          MovePlayer(Vector2.N);
+       } else if (command.Name == "down") {
+          MovePlayer(Vector2.S);
+       } else if (command.Name == "left") {
+          MovePlayer(Vector2.W);
+       } else if (command.Name == "right") {
+          MovePlayer(Vector2.E);
+       } // game ctl      
+       else if (command.Name == "quit") {
+          _levelActive = false;
+       }
+    }
 
 // -------------------------------------------------------------------------
 
-   private void drawItems(IRenderWindow disp) 
+    private void drawItems(IRenderWindow disp) 
     {
-        foreach (var item in _items) 
-           if (_inFov.Contains(item.Pos))
-              item.Draw(disp);
+       foreach (var item in _items) 
+          if (_inFov.Contains(item.Pos))
+             item.Draw(disp);
            
     }
 
     private void drawEnemies(IRenderWindow disp)
     {
-        foreach (var enemy in _enemies)
-           if (_inFov.Contains(enemy.Pos))
-              enemy.Draw(disp);
+       foreach (var enemy in _enemies)
+          if (_inFov.Contains(enemy.Pos))
+             enemy.Draw(disp);
     }
-    
-   private void drawSpecialTiles(IRenderWindow disp)
-   {
-      foreach (var tile in _specialTiles)
-         if (_inFov.Contains(tile.Pos))
-            tile.Draw(disp);
-   }
 
-   private void initMapTileSets(string map) {
-      var lines = map.Split('\n');
+    private void drawSpecialTiles(IRenderWindow disp)
+    {
+       var rng = new Random();
+       foreach (var tile in _specialTiles)
+       {
+          if (_inFov.Contains(tile.Pos))
+             tile.Draw(disp);
+          if (_onPlayer.Contains(tile.Pos))
+          {
+             _player._hp = _player._hp - rng.Next(3, 8);
+             _specialTiles.Remove(tile);
+             break;
+          }
+            
+       }
+         
+      
+    }
 
-    // ------ rules for map ------
-      // . - floor, walkable and transparent.
-      // + - door, walkable and transparent // # - tunnel, walkable and transparent
-      // ' ' - solid stone, not walkable, not transparent.
-      // '|' - wall, not walkable, not transparent, but discoverable.'
-      //  others are treated the same as wall.
-      // tunnel, wall, and doorways are decor, once discovered they are visible.
+    private void initMapTileSets(string map) {
+       var lines = map.Split('\n');
 
-      _floor  = new TileSet();
-      _tunnel = new TileSet();
-      _door   = new TileSet();
-      _decor  = new TileSet();
+       // ------ rules for map ------
+       // . - floor, walkable and transparent.
+       // + - door, walkable and transparent // # - tunnel, walkable and transparent
+       // ' ' - solid stone, not walkable, not transparent.
+       // '|' - wall, not walkable, not transparent, but discoverable.'
+       //  others are treated the same as wall.
+       // tunnel, wall, and doorways are decor, once discovered they are visible.
 
-        foreach (var (c, p) in Vector2.Parse(map))
-        {
-            if (c == '.') _floor.Add(p);
-            else if (c == '+') _door.Add(p);
-            else if (c == '#') _tunnel.Add(p);
-            else if (c != ' ') _decor.Add(p);
-        }
+       _floor  = new TileSet();
+       _tunnel = new TileSet();
+       _door   = new TileSet();
+       _decor  = new TileSet();
 
-        _walkables = _floor.Union(_tunnel).Union(_door).ToHashSet();
+       foreach (var (c, p) in Vector2.Parse(map))
+       {
+          if (c == '.') _floor.Add(p);
+          else if (c == '+') _door.Add(p);
+          else if (c == '#') _tunnel.Add(p);
+          else if (c != ' ') _decor.Add(p);
+       }
+
+       _walkables = _floor.Union(_tunnel).Union(_door).ToHashSet();
 
 //      for (int row = 0; row < lines.Length; ++row) {
 //         for (int col = 0; col < lines[row].Length; ++col) {
@@ -223,48 +241,48 @@ public class Level : Scene
 //            }
 //         }
 //      }
-   }
+    }
 
 // ------------------------------------------------------
 // Commands 
 // ------------------------------------------------------
 
 
-   private void registerCommandsWithScene() {
-      RegisterCommand(ConsoleKey.UpArrow, "up");
-      RegisterCommand(ConsoleKey.W, "up");
-      RegisterCommand(ConsoleKey.K, "up");
+    private void registerCommandsWithScene() {
+       RegisterCommand(ConsoleKey.UpArrow, "up");
+       RegisterCommand(ConsoleKey.W, "up");
+       RegisterCommand(ConsoleKey.K, "up");
 
-      RegisterCommand(ConsoleKey.DownArrow, "down");
-      RegisterCommand(ConsoleKey.S, "down");
-      RegisterCommand(ConsoleKey.J, "down");
+       RegisterCommand(ConsoleKey.DownArrow, "down");
+       RegisterCommand(ConsoleKey.S, "down");
+       RegisterCommand(ConsoleKey.J, "down");
 
-        RegisterCommand(ConsoleKey.LeftArrow, "left");
-        RegisterCommand(ConsoleKey.A, "left");
-        RegisterCommand(ConsoleKey.H, "left");
+       RegisterCommand(ConsoleKey.LeftArrow, "left");
+       RegisterCommand(ConsoleKey.A, "left");
+       RegisterCommand(ConsoleKey.H, "left");
 
-        RegisterCommand(ConsoleKey.RightArrow, "right");
-        RegisterCommand(ConsoleKey.D, "right");
-        RegisterCommand(ConsoleKey.L, "right");
+       RegisterCommand(ConsoleKey.RightArrow, "right");
+       RegisterCommand(ConsoleKey.D, "right");
+       RegisterCommand(ConsoleKey.L, "right");
 
-      RegisterCommand(ConsoleKey.Q, "quit");
-   }
-
-
-   public void MovePlayer(Vector2 delta) {
-      var newPos = _player!.Pos + delta;
-      
-        if (_walkables.Contains(newPos))
-        {
-            var oldPos = _player!.Pos;
-            _player!.Pos = newPos;
-            _walkables.Remove(newPos); // new tile is now occupied
-            _walkables.Add(oldPos);    // old tile is now free
-            updateDiscovered();
-        }
+       RegisterCommand(ConsoleKey.Q, "quit");
     }
 
-   public void QuitLevel() {
-      _levelActive = false;
-   }
+
+    public void MovePlayer(Vector2 delta) {
+       var newPos = _player!.Pos + delta;
+      
+       if (_walkables.Contains(newPos))
+       {
+          var oldPos = _player!.Pos;
+          _player!.Pos = newPos;
+          _walkables.Remove(newPos); // new tile is now occupied
+          _walkables.Add(oldPos);    // old tile is now free
+          updateDiscovered();
+       }
+    }
+
+    public void QuitLevel() {
+       _levelActive = false;
+    }
 }
